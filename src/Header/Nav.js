65ae4f036a4a0c51ce0a9ebcae4ff78e0b0f20c8
Ryan Karumanchery <ryankarumanchery@gmail.com>
2018-11-27 20:43:31 -0500

    Nav Links added

--- src/Header/Nav.js
+++ src/Header/Nav.js
@@ -1,17 +1,34 @@
 import React, { Component } from "react";
-import './Nav.css'
+import { HashRouter } from "react-router-dom";
+import { HashLink as Link } from "react-router-hash-link";
+import "./Nav.css";
 
 class Nav extends Component {
   render() {
     return (
-      <div id='nav'>
-        <h1>Ryan Karumachery</h1>
-        <ul id='right'>
-            <li>Home</li>
-            <li>About</li>
-            <li>Projects</li>
-        </ul>
-      </div>
+      <HashRouter>
+        <div id="nav">
+          <h2>Ryan Karumachery</h2>
+          <div id="right">
+            <Link smooth to="#intro">
+              About
+            </Link>
+            <Link smooth to="#interests">
+              Interests
+            </Link>
+            <Link smooth to="#Work">
+              Work
+            </Link>
+            <Link smooth to="#Technology">
+              Technology
+            </Link>
+            <Link smooth to="#projects">
+              Projects
+            </Link>
+            <a href="#">Resume</a>
+          </div>
+        </div>
+      </HashRouter>
     );
   }
 }
