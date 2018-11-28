010c7630f4b02e3b3d6075853824dfb097e69ce4
Ryan Karumanchery <ryankarumanchery@gmail.com>
2018-11-27 20:42:51 -0500

    Project Page cards added
    - Added pic file
    - Added Project cards
    - Added styling to project cards

--- src/Projects/Projects.js
+++ src/Projects/Projects.js
@@ -1,33 +1,45 @@
-import './Projects.css'
-import React from 'react';
+import "./Projects.css";
+import imgfile from "../Static/project_images/tempfile.jpg";
+import ProjectCard from "./ProjectCard";
+import React from "react";
 
+class Projects extends React.Component {
+  render() {
+    return (
+      <div className="default ProjectStyle">
+        <h1 id="projects" className="title">
+          Projects
+        </h1>
+        <div id='Featured'>
+          <h3 class>Featured Projects</h3>
+          <div className="ProjectCards">
+            <ProjectCard
+              imglink={imgfile}
+              title="Project 1"
+              description="This is a sample description"
+            />
+            <ProjectCard
+              imglink={imgfile}
+              title="Project 2"
+              description="Sample Description"
+            />
+            <ProjectCard
+              imglink={imgfile}
+              title="Project 3"
+              description="Sample Description"
+            />
+          </div>
+        </div>
 
-const ProjectStyle = {
-    backgroundColor: '#385677'
-};
-
-class Projects extends React.Component{
-    render(){
-        return(
-            <div style={ProjectStyle} className="default">
-                <h2 className="title">Projects</h2>
-                <h3>Featured Projects</h3>
-                <ul className="list">
-                    <li>project1</li>
-                    <li>project2</li>
-                    <li>project3</li>
-                    <li>project4</li>
-                </ul>
-
-                <h3>Other Projects</h3>
-                <ul className="list">
-                    <li>Project1</li>
-                    <li>Project2</li>
-                    <li>Project3</li>
-                </ul>
-            </div>
-        )
-    }
+        <h3>Other Projects</h3>
+        <ul className="list">
+          <li>Project1</li>
+          <li>Project2</li>
+          <li>Project3</li>
+        </ul>
+      </div>
+    );
+  }
 }
 
-export default Projects
+export default Projects;
