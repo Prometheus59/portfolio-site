d2a527237985422e27e0872693d8c80f043a7809
Ryan Karumanchery <ryankarumanchery@gmail.com>
2019-01-20 16:36:10 -0500

    Edited icon layouts

--- src/Technology/Technology.js
+++ src/Technology/Technology.js
@@ -1,5 +1,15 @@
 import React, { Component } from "react";
 import "./Technology.css";
+import pythonimg from "./../Static/project_images/python.ico";
+import html5img from "./../Static/project_images/html5.png";
+import css3img from "./../Static/project_images/CSS3.png";
+import javascriptimg from "./../Static/project_images/jslogo.png";
+import cimg from "./../Static/project_images/c.png";
+import githubimg from "./../Static/project_images/git-logooo.png";
+import psqlimg from "./../Static/project_images/psql.png";
+import jqueryimg from "./../Static/project_images/jquery.svg";
+import reactimg from "./../Static/project_images/react.png";
+import SASSimg from "./../Static/project_images/SASS.png";
 
 class Technology extends Component {
   render() {
@@ -8,23 +18,40 @@ class Technology extends Component {
         <div id="">
           <h2 className="title">Programming Languages</h2>
           <ul className="list">
-            <li>HTML5</li>
-            <li>CSS</li>
-            <li>Python</li>
-            <li>JavaScript</li>
-            <li>C</li>
+            <li>
+              <img src={pythonimg} alt="python" />
+            </li>
+            <li>
+              <img src={html5img} alt="html5" />
+            </li>
+            <li>
+              <img src={css3img} alt="css3" />
+            </li>
+            <li>
+              <img src={javascriptimg} alt="javascript" />
+            </li>
+            <li>
+              <img src={cimg} alt="c" />
+            </li>
           </ul>
 
           <h2 className="title">Technical Tools</h2>
           <ul className="list">
-            <li>Git</li>
-            <li>React</li>
-            <li>Sass</li>
-            <li>KnockoutJS</li>
-            <li>JQuery</li>
-            <li>Flask</li>
-            <li>Ajax</li>
-            <li>PSQL</li>
+            <li>
+              <img src={githubimg} alt="github" />
+            </li>
+            <li>
+              <img src={reactimg} alt="react" />
+            </li>
+            <li>
+              <img src={SASSimg} alt="sass" />
+            </li>
+            <li>
+              <img src={jqueryimg} alt="jquery" />
+            </li>
+            <li>
+              <img src={psqlimg} alt="psql" />
+            </li>
           </ul>
         </div>
       </div>
