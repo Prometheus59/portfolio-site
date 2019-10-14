9dfc0d883c04b0c09a5f7cd79cfc4d606f8d30f8
Ryan Karumanchery <ryankarumanchery@gmail.com>
2019-10-14 13:09:20 -0400

    Animated Text - Text animated on about page

--- src/About/About.js
+++ src/About/About.js
@@ -6,12 +6,19 @@ class About extends Component {
     return (
       <div id="About" className="default">
         <div id="intro">
-          <h2 className="header">Hi, I'm Ryan</h2>
-          <p>I'm a junior full-stack web developer with a passion
-            for creating beautiful, responsive web applications in minimal time.</p>
-          <p>
-            I am currently studying computer science at Wilfred Laurier University in Waterloo, Ontario.
-          </p>
+          <h2 className="header focus-in-expand">Hi, I'm Ryan</h2>
+          <div className="info slide-in-left">
+            <p>
+              I'm a junior full-stack web developer with a passion for creating
+              beautiful, responsive web applications in minimal time.
+            </p>
+          </div>
+          <div className="slide-in-left-delay">
+            <p>
+              I am currently studying computer science at Wilfred Laurier
+              University in Waterloo, Ontario.
+            </p>
+          </div>
         </div>
       </div>
     );
