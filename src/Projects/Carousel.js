2b69cd8d2283c2bb43427e4aa1477c86db1225a3
Ryan Karumanchery <ryankarumanchery@gmail.com>
2020-02-05 16:56:41 -0500

    Updated Projects Section

--- src/Projects/Carousel.js
+++ src/Projects/Carousel.js
@@ -4,7 +4,8 @@ import Slider from "react-slick";
 import lucidplatform from "../Static/project_images/lucid-platform-cover.png";
 import wecan from "../Static/project_images/WeCanInsurace.png";
 import wloopoi from "../Static/project_images/poiwaterloo.png";
-import elexicon from "../Static/project_images/elexicon-energy.png";
+import notes from "../Static/project_images/notes.png";
+import cvgenerator from "../Static/project_images/cv-generator.png";
 import ProjectCard from "./ProjectCard";
 import "../../node_modules/slick-carousel/slick/slick-theme.css"; 
 import "../../node_modules/slick-carousel/slick/slick.css";
@@ -25,26 +26,18 @@ class Carousel extends React.Component {
       <Slider {...settings}>
         <div className="resourceCarouselSlides">
           <ProjectCard
-            imglink={elexicon}
-            title="Elexicon Energy Tools"
-            description="Variety of tools for the Credit department"
+            imglink={notes}
+            title="Note Board"
+            description="A note server accessed and modified by clients"
+            githublink="https://github.com/Prometheus59/ClientServer"
+          />
+        </div>
+        <div className="resourceCarouselSlides">
+          <ProjectCard
+            imglink={cvgenerator}
+            title="Cover Letter Generator"
+            description="Tool to create generic/custom/boring cv's"
             githublink="https://github.com/Prometheus59/Elexicon-Energy"
-          />
-        </div>
-        <div className="resourceCarouselSlides">
-          <ProjectCard
-            imglink={lucidplatform}
-            title="Lucid Dreaming Platform"
-            description="Resources for lucid dreaming"
-            githublink="https://github.com/Prometheus59/Lucid-Platform"
-          />
-        </div>
-        <div className="resourceCarouselSlides">
-          <ProjectCard
-            imglink={wecan}
-            title="WeCan Insurance Website"
-            description="Website built for WeCan Insurance"
-            githublink="https://github.com/Prometheus59/WeCanWebsiteV2"
           />
         </div>
         <div className="resourceCarouselSlides">
@@ -53,6 +46,14 @@ class Carousel extends React.Component {
             title="Waterloo POI"
             description="Points of interest around Waterloo, ON"
             githublink="https://github.com/Prometheus59/Waterloo-POI"
+          />
+        </div>
+        <div className="resourceCarouselSlides">
+          <ProjectCard
+            imglink={wecan}
+            title="WeCan Insurance Website"
+            description="Website built for WeCan Insurance"
+            githublink="https://github.com/Prometheus59/WeCanWebsiteV2"
           />
         </div>
       </Slider>
